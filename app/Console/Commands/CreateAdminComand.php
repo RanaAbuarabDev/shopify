39c89817016210824ceba777f7547ad09520d725
Maria <maria.shahla@darrebni.net>
2025-06-03 16:44:46 +0300

    command to add the first admin

---
+++ app/Console/Commands/CreateAdminComand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Admin;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class CreateAdminComand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:create-admin-comand';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        DB::transaction(function () {
+            $admin = Admin::query()->create([]);
+            $name=$this->ask("enter admin name");
+            $email=$this->ask("enter admin email");
+            $password=$this->ask("enter admin password");
+            $admin->user()->create([
+                'name' => $name,
+                'email' => $email,
+                'password' =>$password,
+            ]);
+        });
+        $this->info("Admin created successfully");
+    }
+}
